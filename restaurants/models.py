db8922923d67a2ccd7761f8c2e818f8cff50293f
Christian Bongard <christian.bongard90@gmail.com>
2022-11-24 13:59:49 +0400

    added tables to Restaurant

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=120)
+    location = models.CharField(max_length=120)
+    category = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+class Dish(models.Model):
+    name = models.CharField(max_length=120)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    comment = models.TextField(default='')
+
+    def __str__(self):
+        return self.restaurant.name
