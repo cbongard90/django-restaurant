8ff95608a0d933d3d35891957846c4056b2c8ca9
Christian Bongard <christian.bongard90@gmail.com>
2022-11-25 12:15:57 +0400

    added a detailed view for each restaurant

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+from .models import Restaurant
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('restaurants/index.html')
-    return HttpResponse(template.render({}, request))
+    # template = loader.get_template('restaurants/index.html')
+    all_restaurants = Restaurant.objects.all()
+    context = {
+        'all_restaurants': all_restaurants,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'restaurants/index.html', context)
 
 def detail(request, restaurant_id):
-    return HttpResponse("You're looking at restaurant %s." % restaurant_id)
+    # try:
+    #     restaurant = Restaurant.objects.get(pk=restaurant_id)
+    # except Restaurant.DoesNotExist:
+    #     raise Http404("Restaurant does not exist sorry buddy")
+    restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+    return render(request, 'restaurants/detail.html', {'restaurant': restaurant})
