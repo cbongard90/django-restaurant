f748c659dadc566ad6eb3d690666fdabefb8154c
Christian Bongard <christian.bongard90@gmail.com>
2022-11-25 10:20:54 +0400

    Added index restaurant view

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the restaurants index.")
+    template = loader.get_template('restaurants/index.html')
+    return HttpResponse(template.render({}, request))
+
+def detail(request, restaurant_id):
+    return HttpResponse("You're looking at restaurant %s." % restaurant_id)
